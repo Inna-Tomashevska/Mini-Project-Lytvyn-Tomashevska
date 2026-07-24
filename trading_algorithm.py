624bcb7c260c31eccafca3f668dffb8f8a7f019d
Inna-Tomashevska <inna.tomashevska.25@kse.org.ua>
2026-07-25 02:15:41 +0300

    calculated moving averages

--- trading_algorithm.py
+++ trading_algorithm.py
@@ -10,7 +10,11 @@ data = yf.download(ticker, start="2021-01-01", end="2026-07-25",
 
 data.columns = data.columns.get_level_values(0)
 data = data[["Open", "High", "Low", "Close", "Volume"]]
-coffe_df = pd.DataFrame(data)
-coffe_df = coffe_df.dropna()
+coffee_df = pd.DataFrame(data)
+coffee_df = coffee_df.dropna()
 
-print(coffe_df.head())
+
+coffee_df["20d_Av"] = coffee_df["Close"].rolling(short_window).mean()
+coffee_df["50d_Av"] = coffee_df["Close"].rolling(long_window).mean()
+
+print(coffee_df.tail())
